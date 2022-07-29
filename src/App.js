bc0352c7001b97c4f5fe0ca983bcb4c25bc7113b
github username <olshevskaia.anna@gmail.com>
2022-07-29 08:01:24 -0700

    added google maps with markers

--- src/App.js
+++ src/App.js
@@ -1,53 +1,64 @@
-import React, { useState, useEffect } from "react";
-import './App.css';
-import Search from './SearchForm/Search';
-import Map from "./Map/Map";
-import Axios from "axios";
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import Search from "./SearchForm/Search"
+import Map from "./Map/Map"
+import Axios from "axios"
 
 function App() {
+  const [userInput, setUserInput] = useState("")
+  const [colleges, setColleges] = useState([])
 
-  const [userInput, setUserInput] = useState("");
-  const [colleges, setColleges] = useState([]);
-
+  console.log("App colleges ", colleges)
   function handleInputChange(event) {
-    setUserInput(event?.target.value);
+    setUserInput(event?.target.value)
   }
 
   useEffect(() => {
     const timeOutId = setTimeout(() => {
       if (userInput) {
-        searchColleges();
+        searchColleges()
       }
-    }, 500);
-    return () => clearTimeout(timeOutId);
-  }, [userInput]);
+    }, 500)
+    return () => clearTimeout(timeOutId)
+  }, [userInput])
 
   function searchColleges() {
-    const apiKey = "&api_key=X7jq0SMDNZNwIjnOu1WffzeXCZygoGVAJDQXv8xZ";
+    const apiKey = "&api_key=X7jq0SMDNZNwIjnOu1WffzeXCZygoGVAJDQXv8xZ"
     const baseUrl =
-      "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name=";
-    const params = "&fields=school.name,location";
-    Axios.get(baseUrl + userInput + params + apiKey).then(
-      res => {
-        if (res.data.results.length) {
-          setColleges(res.data.results);
-        }
+      "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.name="
+    const params = "&fields=id,school.name,location"
+    Axios.get(baseUrl + userInput + params + apiKey).then((res) => {
+      if (res.data.results.length) {
+        setColleges(formatColleges(res.data.results))
       }
-    );
+    })
+  }
+
+  function formatColleges(colleges) {
+    return colleges.map((college) => {
+      return (college = {
+        id: college.id,
+        location: {
+          lat: college["location.lat"],
+          lng: college["location.lon"],
+        },
+        name: college["school.name"],
+      })
+    })
   }
 
   return (
     <div className="container">
-    <header>
-      <h1>College Locator</h1>
-    </header>
-    <main>
-      <Search userInput={userInput} onInputChange={handleInputChange} />;
-      <Map />
-    </main>
-    <footer>Developed by Anna O.</footer>
-  </div>
-  );
+      <header>
+        <h1>College Locator</h1>
+      </header>
+      <main>
+        <Search userInput={userInput} onInputChange={handleInputChange} />;
+        <Map colleges={colleges} />
+      </main>
+      <footer>Developed by Anna O.</footer>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,7 +1,42 @@
-import React from "react";
-import "./Map.css";
+import React from "react"
+import "./Map.css"
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api"
 
-export default function Map() {
-  // prettier-ignore
-  return <div>map here</div>
+export default function Map(props) {
+  console.log("Map colleges ", props)
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "AIzaSyBVcGLeee4TM--ZQtIyAjL1ZiefbsVSqtU",
+  })
+
+  const mapContainerStyle = {
+    width: "100vw",
+    height: "100vh",
+  }
+
+  const center = props.colleges.length
+    ? props.colleges[0].location
+    : {
+        lat: 39.099724,
+        lng: -94.578331,
+      }
+
+  // <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center}>
+
+  const collegeMarkers = props.colleges.map((college) => {
+    return <Marker key="college.id" position={college.location}></Marker>
+  })
+
+  return isLoaded ? (
+    <section>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={4.5}
+        center={center}
+      >
+        {collegeMarkers.length && collegeMarkers}
+      </GoogleMap>
+    </section>
+  ) : (
+    <></>
+  )
 }
